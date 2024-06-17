3a906d3803cbce48877e0095b52293d3c71fce9f
Hernan Barrales <chinifoo@gmail.com>
2024-06-17 12:57:53 -0400

    Se realizo cambio de Navbar y todavia esta en desarrollo
    Se elimino busqueda en la navbar y se estas realizando los menu desplegables

--- frontend/src/componets/Navbar/Navbar.jsx
+++ frontend/src/componets/Navbar/Navbar.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import './Navbar.css'
-import logo from '../../assets/Iconos/logo.jpg'
-import SearchBar from "../SeachBar/searchBar"
-import { SearchResultsList } from "../SeachBar/SearchResultsList"
-
+import logo from '../../assets/Iconos/logo.png'
+import menu from './Iconos/menu.svg'
+import search from './Iconos/search.svg'
 
 function Navbar() {
   const [results, setResults] = useState([]);
@@ -13,16 +12,42 @@ function Navbar() {
       <div className="navbar_logo">
         <img src={logo} alt="" className="logo"/>
       </div>
-      <div className="navbar_search">
-        <SearchBar setResults={setResults}/>
-        <div className="search_results">
-          {results && results.length > 0 && <SearchResultsList results={results} />}
+      <div className="navbar_links">
+        <details>
+          <summary>Mapa Interactivo</summary>
+          <a href="/mapa-casamiento">Mapa Casamiento</a>
+          <a href="/mapa-sago">Mapa Sago</a>
+        </details>
+        <details>
+          <summary>Nuestro A G</summary>
+          <a href="/nuestro-gremio">Nuestro Gremio</a>
+          <a href="/directorio">Directorio</a>
+          <a href="/historia">Historia</a>
+        </details>
+        <details>
+          <summary>Servicios Noticias</summary>
+          <a href="/gremiales">Gremiales</a>
+          <a href="/agricolas">Agricolas</a>
+          <a href="/columnas-de-opinion">Columnas de Opinion</a>
+          <a href="/estadisticas">Estadisticas</a>
+        </details>
+        <details>
+          <summary>Plan Estratégico</summary>
+          <a href="/mision-y-vision">Mision y Vision</a>
+          <a href="/desafios">Desafios</a>
+        </details>
+        <a href="/contacto">Contacto</a>
+      </div>
+      <div className="navbar_icons">
+        <div className="icon_container">
+          <img src={search} alt="Search" style={{width: '20px', height: '20px'}} />
+        </div>
+        <div className="icon_container">
+          <img src={menu} alt="Menu" style={{width: '20px', height: '20px'}} />
         </div>
       </div>
     </div>
   );
 }
 
-export default Navbar
-
- 
+export default Navbar;
