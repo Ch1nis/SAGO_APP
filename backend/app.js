79002796c1269a2b91780d5b82f7839a3d8ad616
Hernan Barrales <114186271+Ch1nis@users.noreply.github.com>
2025-12-03 11:05:37 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const app = express();
 
 app.use(cors());
